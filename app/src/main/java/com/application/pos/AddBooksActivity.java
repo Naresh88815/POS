c669c7556754d7917271952fc005c6b96edd918d
Naresh88815 <nareshdon88815@gmail.com>
2023-08-25 23:07:27 +0545

    This is code for Lock The Box.

---
+++ app/src/main/java/com/application/pos/AddBooksActivity.java
@@ -0,0 +1,13 @@
+package com.application.pos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddBooksActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_books_activity);
+    }
+}
---
+++ app/src/main/java/com/application/pos/BooksActivity.java
@@ -0,0 +1,14 @@
+package com.application.pos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.books_activity);
+    }
+}
