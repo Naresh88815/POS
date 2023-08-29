890ba8ec29961850c443baabfc25cbd595b57957
Naresh88815 <nareshdon88815@gmail.com>
2023-08-29 20:15:34 +0545

    This is code for Lock The Box.

--- app/src/main/java/com/application/pos/FragmentMore.java
+++ app/src/main/java/com/application/pos/FragmentMore.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,7 @@ public class FragmentMore extends Fragment {
     View view;
 
     AppCompatButton add_books, add_merchandise, add_vendors;
+    LinearLayout holdOrdersBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +23,7 @@ public class FragmentMore extends Fragment {
         add_books = view.findViewById(R.id.add_books);
         add_merchandise = view.findViewById(R.id.add_merchandise);
         add_vendors = view.findViewById(R.id.add_vendors);
+        holdOrdersBtn = view.findViewById(R.id.holdOrdersBtn);
 
         add_books.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +48,16 @@ public class FragmentMore extends Fragment {
                 startActivity(intent);
             }
         });
+
+        holdOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HoldOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         return  view;
     }
 }
---
+++ app/src/main/java/com/application/pos/HoldOrdersActivity.java
@@ -0,0 +1,13 @@
+package com.application.pos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HoldOrdersActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hold_orders);
+    }
+}
